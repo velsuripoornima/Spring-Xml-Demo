814a837d033a807a3aa8d16c4b2dc385ae82a31d
velsuripoornima <velsuripoornima59@gmail.com>
2019-07-11 17:33:56 +0530

    modify code by using the autowire methods

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,13 +1,21 @@
 package main.java.com.stackroute.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Movie {
 
     private Actor actor;
 
+    public Movie() {
+
+    }
     public Movie(Actor actor) {
         this.actor = actor;
     }
 
+//    @Autowired
+//    @Qualifier("ravi")
     public void setActor(Actor actor) {
         this.actor = actor;
     }
