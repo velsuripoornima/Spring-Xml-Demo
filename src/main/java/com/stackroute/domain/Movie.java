b44460b802cabbc89bb7dd960f71486f4b517bc5
velsuripoornima <velsuripoornima59@gmail.com>
2019-07-11 18:12:31 +0530

    adding interfacess to the code

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,9 +1,15 @@
 package main.java.com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
     private Actor actor;
 
@@ -23,5 +29,24 @@ public class Movie {
     public void displayinfo(){
 
         actor.info();
+    }
+
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+        System.out.println("this method implemented by BeanFactoryAware.....");
+
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+        System.out.println("this method implemented by Application context aware ...");
+
+    }
+
+    public void setBeanName(String s) {
+
+        System.out.println("this is implemented by beanawarenme....");
+
     }
 }
