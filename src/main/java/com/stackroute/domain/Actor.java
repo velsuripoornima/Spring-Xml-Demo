f7c68e695c344adfde0a75c89f7e24fc13f6590e
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-07-18 23:42:43 +0530

    Update Actor.java
    modified with comments

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -4,14 +4,15 @@ public class Actor {
 
     private String name,gender;
     private int age;
-
+    
+//create constructor
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
         this.age = age;
     }
 
-
+//creating getters and setters
     public void setName(String name) {
 
         this.name = name;
@@ -29,6 +30,7 @@ public class Actor {
         this.age = age;
     }
 
+    //display onformation about acotr
     public void info(){
 
         System.out.println("Actor name is:"+name+ "\ngender of the actor is :" +gender+ "\nage is :" +age +"\n");
