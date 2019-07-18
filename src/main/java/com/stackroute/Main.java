f592e1cd15fe8869acfb5c26cbd3a10f73b28405
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-07-19 00:01:47 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -9,6 +9,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
         public static void main(String[] args) {
+                //display using the application context
             ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
 
             Movie movie = context.getBean("rammovie", Movie.class);
@@ -28,10 +29,10 @@ public class Main {
             Movie vijaymovie = context.getBean("vijaymovie", Movie.class);
             vijaymovie.displayinfo();
 
-            System.out.println(movie==movie1);
-            System.out.println(ravimovie==ravimovie1);
+            System.out.println(movie==movie1);  //resulting false 
+            System.out.println(ravimovie==ravimovie1); //resulting true
 
-            ((ClassPathXmlApplicationContext)context).close();
+            ((ClassPathXmlApplicationContext)context).close(); //destroy context
 
 
         }
